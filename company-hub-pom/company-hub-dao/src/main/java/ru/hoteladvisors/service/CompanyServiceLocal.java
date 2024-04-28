91dc82923eaf35e6a91d7a4f698e85bbbe105c4a
sul <shuldeshoff@mail.ru>
2024-04-28 13:58:01 +0300

    init app

--- company-hub-pom/company-hub-dao/src/main/java/ru/hoteladvisors/service/CompanyServiceLocal.java
+++ company-hub-pom/company-hub-dao/src/main/java/ru/hoteladvisors/service/CompanyServiceLocal.java
@@ -1,11 +1,10 @@
 package ru.hoteladvisors.service;
 import ru.hoteladvisors.model.Company;
-
 import javax.ejb.Local;
 import java.util.List;
 
-
 @Local
 public interface CompanyServiceLocal {
+
     List<Company> getAll();
 }
