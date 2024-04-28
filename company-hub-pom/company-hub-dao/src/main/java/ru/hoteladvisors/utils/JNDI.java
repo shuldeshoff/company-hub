3f752b4c17514df3b7f58d779d6b0e23d97e88d3
sul <shuldeshoff@mail.ru>
2024-04-28 13:24:07 +0300

    init app

--- company-hub-pom/company-hub-dao/src/main/java/ru/hoteladvisors/utils/JNDI.java
+++
@@ -1,6 +0,0 @@
-package ru.hoteladvisors.utils;
-
-public class JNDI {
-
-    public static final String COMPANY_SERVICE_BEAN_JNDI = "java:global/company-hub-ejb/CompanyServiceBean!ru.hoteladvisors.service.CompanyServiceLocal";
-}
