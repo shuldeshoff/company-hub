374ae6b2585bfaafec0161f04df0ece913c50e72
sul <shuldeshoff@mail.ru>
2024-04-29 16:12:32 +0300

    init commit

---
+++ database/sql/init_db.sql
@@ -0,0 +1,34 @@
+CREATE USER 'company_hub'@'localhost' IDENTIFIED BY '4hw5vkev';
+CREATE DATABASE company_hub_db;
+GRANT ALL PRIVILEGES ON company_hub_db.* TO 'company_hub'@'localhost';
+
+
+CREATE TABLE addresses (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    postcode VARCHAR(100) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    street VARCHAR(100) NOT NULL,
+    house VARCHAR(10) NOT NULL,
+    apartment VARCHAR(5) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE сompanies (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    legal_form INT NOT NULL,
+    address_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (address_id) REFERENCES addresses(id)
+);
+
+CREATE TABLE branches (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    address_id BIGINT,
+    owner_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (address_id) REFERENCES addresses(id),
+    FOREIGN KEY (owner_id) REFERENCES сompanies(id)
+);
+
